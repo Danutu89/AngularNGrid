66f077e0ad3554fe71ed6cfdb42c82c6dba8b3ab
Daniel Ionut <dany89yt@yahoo.com>
2022-04-13 10:04:57 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, GridOptions } from 'ag-grid-community';
 import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
@@ -17,41 +17,60 @@ export class AppComponent {
   @ViewChild('agGrid') agGrid!: AgGridAngular;
 
   defaultColDef: ColDef = {
-        sortable: true,
-        filter: true
-    };
+    sortable: true,
+    filter: true
+  };
 
   columnDefs: ColDef[] = [
-        { field: 'make', rowGroup: true },
-        { field: 'price' }
-    ];
+    { field: 'make', checkboxSelection: true },
+    { field: 'price' }
+  ];
 
   autoGroupColumnDef: ColDef = {
-        headerName: 'Model',
-        field: 'model',
-        cellRenderer: 'agGroupCellRenderer',
-        cellRendererParams: {
-            checkbox: true
-        }
-    };
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  };
+
+  gridOptions: GridOptions = {
+    columnDefs: this.columnDefs,
+    defaultColDef: this.defaultColDef,
+    autoGroupColumnDef: this.autoGroupColumnDef,
+    rowSelection: 'multiple',
+    rowMultiSelectWithClick: true,
+    animateRows: true,
+    onRowSelected: this.onRowSelected.bind(this),
+  }
 
   rowData: Observable<any[]>;
 
   constructor(private http: HttpClient) {
-      this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+    this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  onRowSelected() {
+    var selectionCounts = this.agGrid.api.getSelectedNodes().length;
+    if (selectionCounts > 2 && this.agGrid) {
+      var oldestNode = this.agGrid.api.getSelectedNodes()[0]; // get the first node, to be popped out
+      if (!oldestNode) return;
+      oldestNode.setSelected(false); // causes the above 'not a function' error
+    }
   }
 
   getSelectedRows() {
-        const selectedNodes = this.agGrid.api.getSelectedNodes();
-        const selectedData = selectedNodes.map(node => {
-          if (node.groupData) {
-            return { make: node.key, model: 'Group' };
-          }
-          return node.data;
-        });
-        const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => {
+      if (node.groupData) {
+        return { make: node.key, model: 'Group' };
+      }
+      return node.data;
+    });
+    const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
 
-        alert(`Selected nodes: ${selectedDataStringPresentation}`);
-    }
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 
 }
