b9f93a5ca732fe5211d364889d6a899cb87e8b91
Daniel Ionut <dany89yt@yahoo.com>
2022-04-14 15:45:18 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CellClassParams, ColDef, GridOptions, RowSelectedEvent } from 'ag-grid-community';
+import { CellClassParams, CellStyle, ColDef, GridOptions, RowSelectedEvent } from 'ag-grid-community';
 import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
@@ -21,53 +21,62 @@ export class AppComponent {
     filter: true
   };
 
-/*  
-  columnDefs: ColDef[] = [
-    {
-      field: 'make', checkboxSelection: true, cellStyle: (row: CellClassParams) => {
-        return {};
-      }
-    },
-    { field: 'price' }
-  ]; 
-*/
+  /*  
+    columnDefs: ColDef[] = [
+      {
+        field: 'make', checkboxSelection: true, cellStyle: (row: CellClassParams) => {
+          return {};
+        }
+      },
+      { field: 'price' }
+    ]; 
+  */
+
+  cellRenderer = (row: CellClassParams) => {
+    if (row.node.isSelected()) {
+      return { "background-color": "initial" } as CellStyle;
+    }
+    if (row.node.selectable) {
+      return { "background-color": "initial" } as CellStyle;
+    } else {
+      return { "background-color": "rgb(166 166 166)" } as CellStyle;
+    }
+  }
 
   columnDefs: ColDef[] = [
-    {headerName: 'Period', field: 'period', checkboxSelection: true, cellStyle: (row: CellClassParams) => {
-        return {};
-      }, pinned: 'left'}, //enum('new', 'old');
-    {headerName: 'Match Score',field: 'matchScore', pinned: 'left'}, //number
-    {headerName: 'ID', field: 'id'}, //number
-    {headerName: 'Insured Object', field: 'insuredObject'}, //string
-    {headerName: 'Comment', field: 'comment'}, //string
-    {headerName: 'Comment 2', field: 'comment2'}, //string
-    {headerName: 'Comment 3', field: 'comment3'}, //string
-    {headerName: 'Comment 4', field: 'comment4'}, //string
-    {headerName: 'Country', field: 'country'}, //enum('countryCodes')
-    {headerName: 'Street', field: 'street'}, //string
-    {headerName: 'Street No.', field: 'streetNumber'}, //string
-    {headerName: 'City', field: 'city'}, //string
-    {headerName: 'State', field: 'state'}, //string
-    {headerName: 'ZIP', field: 'zip'}, //number
-    {headerName: 'Total TSI', field: 'totalTsi'}, //number
-    {headerName: '% Change', field: 'totalTsiPercentage'}, //number (percentage)
-    {headerName: 'Total PD', field: 'totalPd'}, //number
-    {headerName: '% Change', field: 'totalPdPercentage'}, //number (percentage)
-    {headerName: 'TSI Building', field: 'tsiBuilding'}, //number
-    {headerName: '% Change', field: 'tsiBuildingPercentage'}, //number (percentage)
-    {headerName: 'TSI Contents', field: 'tsiContents'}, //number
-    {headerName: '% Change', field: 'tsiContentsPercentage'}, //number (percentage)
-    {headerName: 'TSI BI', field: 'tsiBi'}, //number
-    {headerName: '% Change', field: 'tsiBiPercentage'}, //number (percentage)
-    {headerName: 'Longitude', field: 'longitude'}, //number
-    {headerName: 'Latitude', field: 'latitude'}, //number
-    {headerName: 'Occupancy Code', field: 'occupancyCode'}, //number
-    {headerName: 'No. of Buildings', field: 'numberOfBuildings'}, //number
-    {headerName: 'Stories above Ground', field: 'storiesAboveGround'}, //number
-    {headerName: 'Year of Construction', field: 'constructionYear'}, //year
-    {headerName: 'Construction Class NatCat', field: 'constructionClassNatcat'}, //string
-    {headerName: 'Construction Class', field: 'constructionClass'}, //string
-    {headerName: 'Construction Class Scheme+', field: 'constructionClassScheme'}, //string
+    { headerName: 'Period', field: 'period', checkboxSelection: true, cellStyle: this.cellRenderer, pinned: 'left' }, //enum('new', 'old');
+    { headerName: 'Match Score', field: 'matchScore', pinned: 'left', cellStyle: this.cellRenderer }, //number
+    { headerName: 'ID', field: 'id', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Insured Object', field: 'insuredObject', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Comment', field: 'comment', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Comment 2', field: 'comment2', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Comment 3', field: 'comment3', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Comment 4', field: 'comment4', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Country', field: 'country', cellStyle: this.cellRenderer }, //enum('countryCodes')
+    { headerName: 'Street', field: 'street', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Street No.', field: 'streetNumber', cellStyle: this.cellRenderer }, //string
+    { headerName: 'City', field: 'city', cellStyle: this.cellRenderer }, //string
+    { headerName: 'State', field: 'state', cellStyle: this.cellRenderer }, //string
+    { headerName: 'ZIP', field: 'zip', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Total TSI', field: 'totalTsi', cellStyle: this.cellRenderer }, //number
+    { headerName: '% Change', field: 'totalTsiPercentage', cellStyle: this.cellRenderer }, //number (percentage)
+    { headerName: 'Total PD', field: 'totalPd', cellStyle: this.cellRenderer }, //number
+    { headerName: '% Change', field: 'totalPdPercentage', cellStyle: this.cellRenderer }, //number (percentage)
+    { headerName: 'TSI Building', field: 'tsiBuilding', cellStyle: this.cellRenderer }, //number
+    { headerName: '% Change', field: 'tsiBuildingPercentage', cellStyle: this.cellRenderer }, //number (percentage)
+    { headerName: 'TSI Contents', field: 'tsiContents', cellStyle: this.cellRenderer }, //number
+    { headerName: '% Change', field: 'tsiContentsPercentage', cellStyle: this.cellRenderer }, //number (percentage)
+    { headerName: 'TSI BI', field: 'tsiBi', cellStyle: this.cellRenderer }, //number
+    { headerName: '% Change', field: 'tsiBiPercentage', cellStyle: this.cellRenderer }, //number (percentage)
+    { headerName: 'Longitude', field: 'longitude', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Latitude', field: 'latitude', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Occupancy Code', field: 'occupancyCode', cellStyle: this.cellRenderer }, //number
+    { headerName: 'No. of Buildings', field: 'numberOfBuildings', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Stories above Ground', field: 'storiesAboveGround', cellStyle: this.cellRenderer }, //number
+    { headerName: 'Year of Construction', field: 'constructionYear', cellStyle: this.cellRenderer }, //year
+    { headerName: 'Construction Class NatCat', field: 'constructionClassNatcat', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Construction Class', field: 'constructionClass', cellStyle: this.cellRenderer }, //string
+    { headerName: 'Construction Class Scheme+', field: 'constructionClassScheme', cellStyle: this.cellRenderer }, //string
   ];
 
   autoGroupColumnDef: ColDef = {
@@ -87,6 +96,7 @@ export class AppComponent {
     rowMultiSelectWithClick: true,
     animateRows: true,
     onRowSelected: this.onRowSelected.bind(this),
+
   }
 
   rowData: Observable<any[]>;
@@ -99,9 +109,25 @@ export class AppComponent {
   onRowSelected(event: RowSelectedEvent) {
     const selectedRows = this.agGrid.api.getSelectedNodes();
     var selectionCounts = selectedRows.length;
+    console.log(selectionCounts);
+
     this.agGrid.api.forEachNode((node) => {
-      if (selectedRows.some((row) => row.rowIndex !== node.rowIndex))
-        node.selectable = selectionCounts < 2;
+      let selectable = true;
+
+      if (selectedRows.length === 0) {
+        selectable = true;
+      }
+
+      if (selectedRows.some((row) => row.rowIndex !== node.rowIndex)) {
+        if (selectionCounts === 2) selectable = false
+        else selectable = selectedRows[0] ? selectedRows[0].data['period'] !== node.data['period'] : true;
+
+      }
+      node.selectable = selectable;
+    });
+    this.agGrid.api.refreshCells({
+      force: true,
+      suppressFlash: true
     });
   }
 
